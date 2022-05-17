bc2639f5d6fd33d166b83783af6a3e54d43e8105
S1GARETA <rabota.dsv@mail.ru>
2022-05-17 16:30:49 +0500

    add templates

--- Credits/urls.py
+++ Credits/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='main'),
     path('login', views.login, name='login'),
+    path('guide', views.guide, name='guide'),
+    path('soon', views.soon, name='soon'),
     path('test', views.test)
 ]
--- Credits/views.py
+++ Credits/views.py
@@ -3,11 +3,24 @@ from .models import TestDBUser
 
 
 def index(request):
-    return render(request, 'credits/index.html')
+    print(str(request.user))
+    if (str(request.user) != 'AnonymousUser'):
+        return render(request, 'credits/index.html', {'username': request.user})
+    else:
+        return login(request)
 
 def login(request):
     return render(request, 'credits/registration.html')
 
+def guide(request):
+    return render(request, 'credits/tytorial.html', {'username': request.user})
+
+def soon(request):
+    return render(request, 'credits/soon.html')
+
+
+
 def test(request):
-    tests = TestDBUser.objects.all()
-    return render(request, 'credits/testDB.html', {'tests': tests, 'user': request.user})
+    # tests = TestDBUser.objects.all()
+    # , {'tests': tests, 'user': request.user}
+    return render(request, 'credits/testDB.html')
