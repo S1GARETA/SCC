cd4f189be0bb7cea729101dfdcace355e53399d7
S1GARETA <rabota.dsv@mail.ru>
2022-06-09 20:56:05 +0500

    deleted backend

--- backend/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from backend.forms import UserForm
-
-
-# def register(request):
-#     if request.method == 'POST':
-#         pass
-#     user_form = UserForm()
-#     return render(request, 'index.html', {'user_form': user_form})
